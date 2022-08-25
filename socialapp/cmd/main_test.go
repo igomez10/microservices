621cb41f6e637be8ae7c6cb357a46a018a246058
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2022-08-25 14:08:24 +0200

    add test to benchmark create user

--- socialapp/cmd/main_test.go
+++ socialapp/cmd/main_test.go
@@ -3,10 +3,16 @@ package main
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"socialapp/pkg/controller/user"
 	"socialapp/pkg/db"
+	"socialapp/socialappapi/openapi"
+	"sync"
 	"testing"
+	"time"
 
 	_ "github.com/lib/pq"
+	"github.com/rs/zerolog/log"
 )
 
 // integration
@@ -47,4 +53,40 @@ func TestFetchURLIntegration(t *testing.T) {
 		t.Error(actualUser, createUserReq)
 	}
 
+}
+
+func TestCreateUsers(t *testing.T) {
+	dbConn, err := sql.Open("postgres", "postgres://postgres:password@localhost:5433/postgres?sslmode=disable")
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	defer dbConn.Close()
+
+	if dbConn == nil {
+		log.Fatal().Msg("db is nil")
+	}
+	defer dbConn.Close()
+
+	queries := db.New()
+
+	ctx := context.Background()
+	UserApiService := &user.UserApiService{DB: queries, DBConn: dbConn}
+	for {
+		wg := sync.WaitGroup{}
+		numUsers := 500
+		wg.Add(numUsers)
+		for i := 0; i < numUsers; i++ {
+			go func(i int) {
+				UserApiService.CreateUser(ctx, openapi.User{
+					Username:  fmt.Sprintf("Test-%d-%d", time.Now().UnixNano(), i),
+					FirstName: "first",
+					LastName:  "last",
+					Email:     fmt.Sprintf("Test-%d-%d@test.com", time.Now().UnixNano(), i),
+				})
+				wg.Done()
+				// time.Sleep(10 * time.Millisecond)
+			}(i)
+		}
+		wg.Wait()
+	}
 }
