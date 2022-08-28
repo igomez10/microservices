a55f29070ffa98b6bd1ab80f81ee81516ab0a815
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2022-08-28 18:40:56 +0200

    add flags for ports

--- socialapp/cmd/main.go
+++ socialapp/cmd/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"flag"
+	"fmt"
 	"net/http"
 	"socialapp/pkg/controller/comment"
 	"socialapp/pkg/controller/user"
@@ -13,9 +15,15 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+var (
+	appPort  *int = flag.Int("port", 8080, "main port for application")
+	metaPort *int = flag.Int("meta_port", 9095, "meta port for metric/service_discovery/etc")
+)
+
 func main() {
-	go startPrometheusServer()
-	log.Printf("Server started")
+	flag.Parse()
+	log.Info().Msgf("Starting PORT: %d, METAPORT: %d", *appPort, *metaPort)
+	go startPrometheusServer(*metaPort)
 	dbConn, err := sql.Open("postgres", "postgres://postgres:password@localhost:5433/postgres?sslmode=disable")
 	if err != nil {
 		log.Fatal().Err(err)
@@ -29,20 +37,24 @@ func main() {
 
 	queries := db.New()
 
+	// Comment service
 	CommentApiService := &comment.CommentService{DB: queries, DBConn: dbConn}
 	CommentApiController := openapi.NewCommentApiController(CommentApiService)
 
+	// User service
 	UserApiService := &user.UserApiService{DB: queries, DBConn: dbConn}
 	UserApiController := openapi.NewUserApiController(UserApiService)
 
 	router := openapi.NewRouter(CommentApiController, UserApiController)
-	log.Debug().Msg("Server is listening on port :8080")
-	log.Fatal().Err(http.ListenAndServe(":8080", router))
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", *appPort), router); err != nil {
+		log.Fatal().Err(err).Msgf("Shutting down")
+	}
+
 }
 
-func startPrometheusServer() error {
-	addr := ":9095"
+func startPrometheusServer(port int) error {
+
 	// Expose the registered metrics via HTTP.
 	http.Handle("/metrics", promhttp.Handler())
-	return http.ListenAndServe(addr, nil)
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
