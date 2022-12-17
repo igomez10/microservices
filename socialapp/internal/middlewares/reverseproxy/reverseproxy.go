9dc74b5c9f75cf199647fb0af3e533d12958a7ec
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2022-12-17 11:42:28 -0500

    add metrics with host

--- socialapp/internal/middlewares/reverseproxy/reverseproxy.go
+++ socialapp/internal/middlewares/reverseproxy/reverseproxy.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -13,26 +14,30 @@ type ReverseProxy struct {
 	KibanaURL url.URL
 }
 
-var prometheusProxyRequests = promauto.NewCounter(prometheus.CounterOpts{
-	Name: "proxy_requests",
-	Help: "Number of requests to the proxy",
-})
+var prometheusProxyRequests = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "proxy_requests_total",
+	Help: "The total number of proxy requests",
+}, []string{"host"})
+var prometheusProxyResponseTime = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	Name: "proxy_response_time_milliseconds",
+	Help: "The response time of proxy requests",
+}, []string{"host"})
 
 func (r *ReverseProxy) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		hostHeader := req.Host
 		if hostHeader == "kibana.gomezignacio.com" {
 			// metrics for proxy
-			prometheusProxyRequests.Inc()
-
+			prometheusProxyRequests.WithLabelValues(hostHeader).Inc()
+			startTime := time.Now()
 			proxy := httputil.NewSingleHostReverseProxy(&url.URL{
 				Scheme: r.KibanaURL.Scheme,
 				Host:   r.KibanaURL.Host,
 			})
-
 			proxy.ServeHTTP(w, req)
-			return
+			prometheusProxyResponseTime.WithLabelValues(hostHeader).Observe(float64(time.Since(startTime).Milliseconds()))
 
+			return
 		}
 		// ---------
 		//  HANDLE REQUEST
