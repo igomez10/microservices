99be9e492abe8b46370de182a9e009c2ab9bc860
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2022-12-17 11:39:56 -0500

    add prometheus metrics for proxy requests

--- socialapp/internal/middlewares/reverseproxy/reverseproxy.go
+++ socialapp/internal/middlewares/reverseproxy/reverseproxy.go
@@ -4,16 +4,26 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 type ReverseProxy struct {
 	KibanaURL url.URL
 }
 
+var prometheusProxyRequests = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "proxy_requests",
+	Help: "Number of requests to the proxy",
+})
+
 func (r *ReverseProxy) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		hostHeader := req.Host
 		if hostHeader == "kibana.gomezignacio.com" {
+			// metrics for proxy
+			prometheusProxyRequests.Inc()
 
 			proxy := httputil.NewSingleHostReverseProxy(&url.URL{
 				Scheme: r.KibanaURL.Scheme,
