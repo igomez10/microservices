2109256d4d17d55e2c2cfb3fad5ab2e45aa59856
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2023-02-11 14:40:40 -0500

    update prom metric value

--- socialapp/internal/middlewares/gandalf/gandalf.go
+++ socialapp/internal/middlewares/gandalf/gandalf.go
@@ -23,11 +23,12 @@ var gandalf_token_cache = promauto.NewCounterVec(prometheus.CounterOpts{
 	Help: "The total number of gandalf token cache",
 }, []string{"cache", "status"})
 
-var gandalf_duration_seconds = promauto.NewHistogramVec(prometheus.HistogramOpts{
-	Name:    "gandalf_duration_seconds",
-	Help:    "The duration of gandalf requests",
-	Buckets: prometheus.LinearBuckets(0.1, 0.1, 10),
-}, []string{"status"})
+// gandalf_duration_milliseconds The duration of gandalf middleware in milliseconds
+var gandalf_duration_milliseconds = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	Name:    "gandalf_duration_milliseconds",
+	Help:    "The duration of gandalf middleware in milliseconds",
+	Buckets: prometheus.LinearBuckets(0, 10, 100),
+}, []string{"auth_result"})
 
 type Middleware struct {
 	DB               db.Querier
@@ -42,7 +43,7 @@ func (m *Middleware) Authenticate(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		authResult := "failure"
+		var authResult string
 		log := contexthelper.GetLoggerInContext(r.Context())
 		// get token from header
 		if m.AllowlistedPaths[r.URL.Path] != nil && m.AllowlistedPaths[r.URL.Path][r.Method] {
@@ -331,7 +332,8 @@ func (m *Middleware) Authenticate(next http.Handler) http.Handler {
 			}
 		}
 
-		gandalf_duration_seconds.WithLabelValues(authResult).Observe(time.Since(start).Seconds())
+		gandalf_duration_milliseconds.WithLabelValues(authResult).Observe(float64(time.Since(start).Milliseconds()))
+
 		next.ServeHTTP(w, r)
 	})
 
