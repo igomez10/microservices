987fa581ce578b95dab323ac4299077bb86c8606
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2023-02-18 14:49:09 +0000

    add puttyknife collector for debezium

---
+++ socialapp/debezium/setup.sh
@@ -0,0 +1,11 @@
+# SOCIALAPP
+curl -i -X POST -H "Accept:application/json" \
+    -H  "Content-Type:application/json" \
+    http://localhost:8083/connectors/ \
+    -d @./debezium/config/socialapp-register-postgres.json
+
+# PUTTYKNIFE
+curl -i -X POST -H "Accept:application/json" \
+-H  "Content-Type:application/json" \
+http://localhost:8083/connectors/ \
+-d @./debezium/config/puttyknife-register-postgres.json
