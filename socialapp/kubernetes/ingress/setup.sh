dd0dec7c32a5e8cff69fa16b463103760850590b
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2023-06-26 00:20:45 +0200

    add ingress with root cert

--- socialapp/kubernetes/ingress/setup.sh
+++ socialapp/kubernetes/ingress/setup.sh
@@ -1,7 +1,21 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.0/deploy/static/provider/cloud/deploy.yaml
-kubectl delete secret selfsigned-cert
-openssl genrsa -out server.key 2048
+# openssl genrsa -out server.key 2048
 # for cert at website example.com
-openssl req -new -x509 -sha256 -key server.key -out server.crt \
+openssl req -new -x509 -sha256 -key server.key -out asteriskserver.crt \
+-days 3650 -subj "/CN=*.example.com" -addext "subjectAltName=DNS:*.example.com,DNS:www.*.example.com"
+
+openssl req -new -x509 -sha256 -key server.key -out prometheusserver.crt \
 -days 3650 -subj "/CN=prometheus.example.com" -addext "subjectAltName=DNS:prometheus.example.com,DNS:www.prometheus.example.com"
-kubectl create secret tls selfsigned-cert --key server.key --cert server.crt -n default
+
+openssl req -new -x509 -sha256 -key server.key -out grafana.crt \
+-days 3650 -subj "/CN=grafana.example.com" -addext "subjectAltName=DNS:grafana.example.com,DNS:www.grafana.example.com"
+
+# delete secrets first
+kubectl delete secret prometheus-cert
+kubectl delete secret grafana-cert
+kubectl delete secret asterisk-cert
+
+# create secrets
+kubectl create secret tls prometheus-cert --key server.key --cert prometheusserver.crt -n default
+kubectl create secret tls grafana-cert --key server.key --cert grafana.crt -n default
+kubectl create secret tls asterisk-cert --key server.key --cert asteriskserver.crt -n default
