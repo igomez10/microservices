8805d49b33d02bd0df8c6e05f4ed72e66a6335a2
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2024-01-25 05:35:00 +0000

    add meta router

--- urlshortener/cmd/main.go
+++ urlshortener/cmd/main.go
@@ -8,14 +8,21 @@ import (
 	"os"
 	"time"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/igomez10/microservices/urlshortener/generated/server"
+	"github.com/igomez10/microservices/urlshortener/pkg/contexthelper"
 	"github.com/igomez10/microservices/urlshortener/pkg/controllers/url"
 	"github.com/igomez10/microservices/urlshortener/pkg/db"
 	flags "github.com/jessevdk/go-flags"
 	_ "github.com/newrelic/go-agent/v3/integrations/nrpq"
 	"github.com/newrelic/go-agent/v3/newrelic"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/slok/go-http-metrics/metrics/prometheus"
+	metricsMiddleware "github.com/slok/go-http-metrics/middleware"
+	"github.com/slok/go-http-metrics/middleware/std"
 )
 
 var opts struct {
@@ -86,13 +93,15 @@ func main() {
 
 	// Start meta service
 	go func() {
-		if err := http.ListenAndServe(fmt.Sprintf("%s:%d", opts.MetaServer.Addr, opts.MetaServer.Port), nil); err != nil {
+		meta := NewMetaRouter()
+		log.Info().Str("addr", fmt.Sprintf("%s:%d", opts.MetaServer.Addr, opts.MetaServer.Port)).Msg("starting meta service")
+		if err := http.ListenAndServe(fmt.Sprintf("%s:%d", opts.MetaServer.Addr, opts.MetaServer.Port), meta); err != nil {
 			log.Fatal().Err(err).Msg("failed to start meta service")
 		}
 	}()
 
 	// start new relic
-	// var newrelicApp *newrelic.Application
+	var newrelicApp *newrelic.Application
 	if opts.NewRelicLicense != "" {
 		_, err := newrelic.NewApplication(
 			newrelic.ConfigAppName("urlshortener"),
@@ -107,10 +116,104 @@ func main() {
 	}
 
 	// Start HTTP server
-	urlRouter := server.NewRouter(URLAPIController)
+	middlewares := []func(http.Handler) http.Handler{
+		middleware.RequestID,
+		middleware.RealIP,
+		middleware.Recoverer,
+	}
+	urlRouter := NewRouter(middlewares, []server.Router{URLAPIController}, newrelicApp)
+
 	addr := fmt.Sprintf("%s:%d", opts.HTTPAddr, opts.Port)
 	log.Info().Str("addr", addr).Msg("starting HTTP server")
 	if err := http.ListenAndServe(addr, urlRouter); err != nil {
 		log.Fatal().Err(err).Msg("failed to start HTTP server")
 	}
+}
+
+type Pattern struct {
+	Pattern string
+}
+
+func NewMetaRouter() chi.Router {
+	mainRouter := chi.NewRouter()
+
+	mainRouter.Mount("/debug", middleware.Profiler())
+	// Expose health the registered metrics via HTTP, no logging for those requests
+	mainRouter.Group(func(r chi.Router) {
+		// HEALTH
+		r.MethodFunc("GET", "/health", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("OK"))
+		})
+
+		// METRICS
+		r.Handle("/metrics", promhttp.Handler())
+
+		// OPENAPI
+		// Expose the api spec via HTTP.
+		r.HandleFunc("/apispec", func(w http.ResponseWriter, r *http.Request) {
+			// send open api file
+			// open api file
+			file := "openapi.yaml"
+			content, err := os.ReadFile(file)
+			if err != nil {
+				log.Error().Err(err).Msg("Error reading file")
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			w.Write(content)
+		})
+	})
+
+	return mainRouter
+}
+
+// Adds the request pattern into the context, this is only required because chi.mux
+// does not provide a way to get the pattern from the request. This middleware will update the
+// string pointer saved in the context as "pattern"
+func (p *Pattern) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r = r.WithContext(contexthelper.SetRequestPatternInContext(r.Context(), p.Pattern))
+		next.ServeHTTP(w, r)
+	})
+}
+
+// NewRouter creates a new router for any number of api routers
+func NewRouter(middlewares []func(http.Handler) http.Handler, routers []server.Router, newrelicApp *newrelic.Application) chi.Router {
+	mainRouter := chi.NewRouter()
+
+	// Main router group for api logic
+	mainRouter.Group(func(r chi.Router) {
+
+		mdlw := metricsMiddleware.New(metricsMiddleware.Config{
+			Recorder: prometheus.NewRecorder(prometheus.Config{}),
+		})
+
+		for _, api := range routers {
+			for _, route := range api.Routes() {
+				var handler http.Handler
+				handler = route.HandlerFunc
+
+				r.Group(func(r chi.Router) {
+					// use a  custom middleware to record the metrics on the route pattern.
+					r.Use(std.HandlerProvider(route.Pattern, mdlw))
+
+					pattern := Pattern{Pattern: route.Pattern}
+					r.Use(pattern.Middleware)
+
+					for i := range middlewares {
+						r.Use(middlewares[i])
+					}
+
+					if newrelicApp != nil {
+						_, handler = newrelic.WrapHandle(newrelicApp, route.Pattern, handler)
+					}
+
+					r.Method(route.Method, route.Pattern, handler)
+				})
+			}
+		}
+	})
+
+	return mainRouter
 }
--- urlshortener/go.mod
+++ urlshortener/go.mod
@@ -7,20 +7,30 @@ require (
 	github.com/go-chi/cors v1.2.1
 	github.com/google/uuid v1.5.0
 	github.com/jessevdk/go-flags v1.5.0
+	github.com/newrelic/go-agent/v3 v3.24.1
 	github.com/newrelic/go-agent/v3/integrations/nrpq v1.1.1
+	github.com/prometheus/client_golang v1.18.0
 	github.com/rs/zerolog v1.30.0
+	github.com/slok/go-http-metrics v0.11.0
 	golang.org/x/oauth2 v0.15.0
+	honnef.co/go/tools v0.4.6
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/newrelic/go-agent/v3 v3.24.1 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.12.1-0.20230825192346-2191a27a6dc5 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
 	google.golang.org/grpc v1.57.0 // indirect
