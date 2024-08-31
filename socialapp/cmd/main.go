478e8f3d1fcd9a74c0c632a6fc4ebc9b165b3008
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2024-08-31 19:09:01 +0200

    chore: Add OpenTelemetry text map propagator and update dependencies

--- socialapp/cmd/main.go
+++ socialapp/cmd/main.go
@@ -51,6 +51,7 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
