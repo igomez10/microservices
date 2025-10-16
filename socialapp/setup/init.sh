5e54bc8060715fcd340fe73bd652123972f7b775
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2025-10-16 23:54:20 +0000

    feat: add role and user creation for logstash service account in init script

--- socialapp/setup/init.sh
+++ socialapp/setup/init.sh
@@ -12,5 +12,33 @@ curl -X POST "http://elasticsearch:9200/_security/role/logstash_writer" \
     ]
   }'
 
+# create role property_writer 
+curl -X POST "http://elasticsearch:9200/_security/role/property_writer" \
+  -H "Content-Type: application/json" \
+  -u "elastic:${ELASTIC_PASSWORD}" \
+  -d '{
+    "cluster": ["all"],
+    "indices": [
+      {
+        "names": ["properties*"],
+        "privileges": ["all"]
+      }
+    ]
+  }'
+
+# create user logstash_service_account
+curl -X POST "http://elasticsearch:9200/_security/user/logstash_service_account" \
+  -H "Content-Type: application/json" \
+  -u "elastic:${ELASTIC_PASSWORD}" \
+  -d '{
+    "password": "'"${LOGSTASH_HOST_SERVICE_ACCOUNT_PASSWORD}"'",
+    "full_name": "Logstash Service Account",
+    "email": "logstash_service_account@example.com",
+    "metadata": {
+      "intake": "logstash"
+    },
+    "roles": ["logstash_writer", "property_writer"]
+  }'
+
 psql -U postgres -h database -d postgres -c "CREATE DATABASE socialapp;"
 psql -U postgres -h database -d socialapp -f /schema.sql
