1a8e48ff62a3bb01f93218fd95035d813a1319cb
Ignacio Gomez <igomez9@dons.usfca.edu>
2025-10-29 10:45:38 -0700

    test: add TestMultiLevelLog to verify logging across multiple outputs

--- socialapp/cmd/main_test.go
+++ socialapp/cmd/main_test.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"net"
 	"testing"
+	"time"
 
 	db "github.com/igomez10/microservices/socialapp/pkg/dbpgx"
 	"github.com/jackc/pgx/v5"
 	_ "github.com/lib/pq"
+	"github.com/rs/zerolog"
 )
 
 // TestFetchURLIntegration tests the integration of the fetchURL function
@@ -90,3 +94,56 @@ func TestFetchURLIntegration(t *testing.T) {
 // 		time.Sleep(200 * time.Millisecond)
 // 	}
 // }
+
+func TestMultiLevelLog(t *testing.T) {
+	memoryFile := bytes.NewBuffer([]byte{})
+	secondFile := bytes.NewBuffer([]byte{})
+	server, err := net.ListenTCP("tcp", &net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: 0})
+	if err != nil {
+		t.Fatal(err)
+	}
+	streamBytes := bytes.NewBuffer([]byte{})
+	go func() {
+		conn, err := server.Accept()
+		if err != nil {
+			t.Log(err)
+			return
+		}
+		defer conn.Close()
+		buf := make([]byte, 1024)
+		for {
+			n, err := conn.Read(buf)
+			if err != nil {
+				t.Log(err)
+				return
+			}
+			streamBytes.Write(buf[:n])
+		}
+	}()
+	defer server.Close()
+	stream, err := net.DialTCP("tcp", nil, server.Addr().(*net.TCPAddr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer stream.Close()
+
+	ml := zerolog.MultiLevelWriter(memoryFile, secondFile, stream)
+	logger := zerolog.New(ml).With().Timestamp().Logger()
+
+	logger.Info().Msg("info message")
+
+	// Give the goroutine time to read from the stream
+	time.Sleep(100 * time.Millisecond)
+
+	if !bytes.Contains(memoryFile.Bytes(), []byte("info message")) {
+		t.Error("info message not found in memoryFile")
+	}
+
+	if !bytes.Contains(secondFile.Bytes(), []byte("info message")) {
+		t.Error("info message not found in secondFile")
+	}
+
+	if !bytes.Contains(streamBytes.Bytes(), []byte("info message")) {
+		t.Error("info message not found in stream")
+	}
+}
