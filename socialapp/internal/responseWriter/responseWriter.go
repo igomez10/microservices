7e553b52d010482c73c5fcefffb072d25fa2c772
Ignacio Gomez <igomez9@dons.usfca.edu>
2025-10-31 21:30:13 -0700

    feat: enhance custom response writer to prevent duplicate WriteHeader calls and ensure proper header handling

--- socialapp/internal/responseWriter/responseWriter.go
+++ socialapp/internal/responseWriter/responseWriter.go
@@ -1,24 +1,42 @@
 package responseWriter
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/rs/zerolog/log"
+)
 
 // custom response writer for capturing status code in the response
 type customResponseWriter struct {
 	http.ResponseWriter
-	StatusCode int
-	Body       []byte
+	StatusCode  int
+	Body        []byte
+	wroteHeader bool
 }
 
 func NewCustomResponseWriter(w http.ResponseWriter) *customResponseWriter {
-	return &customResponseWriter{w, http.StatusOK, []byte{}}
+	return &customResponseWriter{
+		ResponseWriter: w,
+		StatusCode:     http.StatusOK,
+		Body:           []byte{},
+		wroteHeader:    false,
+	}
 }
 
 func (lrw *customResponseWriter) WriteHeader(code int) {
+	if lrw.wroteHeader {
+		log.Error().Stack().Msg("WriteHeader called twice")
+		return // Prevent duplicate WriteHeader calls
+	}
+	lrw.wroteHeader = true
 	lrw.StatusCode = code
 	lrw.ResponseWriter.WriteHeader(code)
 }
 
 func (lrw *customResponseWriter) Write(b []byte) (int, error) {
+	if !lrw.wroteHeader {
+		lrw.WriteHeader(http.StatusOK)
+	}
 	lrw.Body = append(lrw.Body, b...)
 	return lrw.ResponseWriter.Write(b)
 }
