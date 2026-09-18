86a6e9091453080b352e91078b8eb8c5d5cf4721
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2026-09-17 20:00:24 -0700

    refactor: remove deprecated Kubernetes resources for database, zookeeper, and monitoring services

--- socialapp/kubernetes/prometheus/setup.sh
+++
@@ -1,7 +0,0 @@
-for file in $(find prometheus -type f)
-do
-    filename=$(basename "$file")
-    configmap_name="prometheus-config-${filename}"
-    kubectl create configmap "$configmap_name" --from-file="$file"
-    kubectl label configmap $configmap_name app=prometheus
-done
